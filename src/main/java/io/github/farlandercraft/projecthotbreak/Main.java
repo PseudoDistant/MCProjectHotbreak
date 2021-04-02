4e5274fb2d4bc8f234be539271ed96a0df21df4b
FarlanderCraft <javafarlander2002@gmail.com>
2021-04-01 21:09:54 -0400

    Changed onInitialize message

--- src/main/java/io/github/farlandercraft/projecthotbreak/Main.java
+++ src/main/java/io/github/farlandercraft/projecthotbreak/Main.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class Main implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		System.out.println("You have successfully unpatched a few fun Minecraft bugs!!");
+		System.out.println("Hotbreak has loaded successfully!");
 	}
 }
