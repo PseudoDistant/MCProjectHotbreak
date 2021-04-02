0cc0e355d1d38d72e696c4f423f646fc159fdb24
FarLanderCraft <67613281+FarLanderCraft@users.noreply.github.com>
2021-04-01 21:03:02 -0400

    Delete src/main/java/io/github/farlandercraft/projecthotbreak/iface directory

--- src/main/java/io/github/farlandercraft/projecthotbreak/iface/ChunkManagerExtra.java
+++
@@ -1,9 +0,0 @@
-// vim: noet
-package io.github.farlandercraft.projecthotbreak.iface;
-
-import net.minecraft.server.world.ChunkTicketManager;
-import net.minecraft.util.math.ChunkPos;
-
-public interface ChunkManagerExtra {
-	public void levelUpdate(ChunkPos pos);
-}
